60da244a53b785177b1d21fddc85045fb994563c
Icaro Vieira <icarovsilva1@gmail.com>
2023-06-04 14:07:48 -0300

    feat: create recursive search

--- cmd/tree_left_and_right/main.go
+++ cmd/tree_left_and_right/main.go
@@ -7,6 +7,7 @@ import (
 
 type Tree struct {
 	Name  string
+	Id    int
 	Time  time.Time
 	left  *Tree
 	right *Tree
@@ -16,6 +17,7 @@ func main() {
 
 	first := &Tree{
 		Name:  "first",
+		Id:    1,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
@@ -23,37 +25,43 @@ func main() {
 
 	second := &Tree{
 		Name:  "second",
+		Id:    2,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
 	}
 	third := &Tree{
 		Name:  "third",
+		Id:    3,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
 	}
 	fourth := &Tree{
 		Name:  "fourth",
+		Id:    4,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
 	}
 	five := &Tree{
 		Name:  "five",
+		Id:    5,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
 	}
 
 	left := &Tree{
-		Name:  "First left",
+		Name:  "left",
+		Id:    6,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
 	}
 	right := &Tree{
-		Name:  "First left right",
+		Name:  "right",
+		Id:    7,
 		Time:  time.Now(),
 		left:  nil,
 		right: nil,
@@ -78,8 +86,18 @@ func main() {
 	*/
 
 	walkOnTree(first)
-	findNameOnTree(first, "First left right")
+	findNameOnTree(first, "right")
+	finded := findNameOnTreeRecursive(first, "right")
 
+	fmt.Println("Finded:", finded)
+
+	result := walkOnTreeRecursive(first)
+
+	for _, v := range result {
+		fmt.Println(*v)
+	}
+
+	breadthFirstValues(first)
 }
 
 func walkOnTree(t *Tree) {
@@ -96,8 +114,27 @@ func walkOnTree(t *Tree) {
 		if node.right != nil {
 			stack = append(stack, node.right)
 		}
-		fmt.Println(node.Name)
+		fmt.Printf("Node name:%s . Node id: %d\n", node.Name, node.Id)
 	}
+}
+
+func walkOnTreeRecursive(t *Tree) []*Tree {
+	stack := []*Tree{t}
+
+	if t == nil {
+		return nil
+	}
+
+	left := walkOnTreeRecursive(t.left)
+	right := walkOnTreeRecursive(t.right)
+
+	// stack = append(stack, walkOnTreeRecursive(t.left)...)
+	// stack = append(stack, walkOnTreeRecursive(t.right)...)
+
+	stack = append(stack, left...)
+	stack = append(stack, right...)
+
+	return stack
 }
 
 func findNameOnTree(t *Tree, name string) {
@@ -111,8 +148,7 @@ func findNameOnTree(t *Tree, name string) {
 		stack = stack[:len(stack)-1]
 
 		if node.Name == name {
-			fmt.Println(iterations)
-			fmt.Println(node.Name)
+			fmt.Printf("Iterations: %d\nNode name: %s . Node id: %d\n", iterations, node.Name, node.Id)
 			return
 		}
 
@@ -124,4 +160,44 @@ func findNameOnTree(t *Tree, name string) {
 		}
 		iterations++
 	}
+}
+
+func findNameOnTreeRecursive(t *Tree, name string) bool {
+
+	if t.Name == name {
+		fmt.Println(" Node name:", t.Name, "Node id:", t.Id)
+		return true
+	}
+	if t.left == nil {
+		return false
+	}
+	if t.right == nil {
+		return false
+	}
+
+	return findNameOnTreeRecursive(t.right, name) || findNameOnTreeRecursive(t.left, name)
+}
+
+func breadthFirstValues(t *Tree) {
+
+	stack := []*Tree{t}
+	walk := []string{}
+
+	for len(stack) > 0 {
+
+		node := stack[0]
+		walk = append(walk, node.Name)
+		stack = stack[1:]
+
+		if node.right != nil {
+			stack = append(stack, node.right)
+		}
+		if node.left != nil {
+			stack = append(stack, node.left)
+		}
+
+	}
+
+	fmt.Println(walk)
+
 }
